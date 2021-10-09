5994a5fbf5b1c87727567e6d37ae9de258b704bd
nightmareci <nightmareci@gmail.com>
2021-10-08 17:28:01 -0700

    Uncomment the other examples (oops)

--- build_all_linux.sh
+++ build_all_linux.sh
@@ -3,12 +3,12 @@
 mkdir linux
 
 gcc -o burn.bin source/burn.c source/dos.c -lSDL2 -lGLEW -lGL -lm -lpthread
-#gcc -o julia.bin source/julia.c source/dos.c -lSDL2 -lGLEW -lGL -lm -lpthread
-#gcc -o mandelbrot.bin source/mandelbrot.c source/dos.c -lSDL2 -lGLEW -lGL -lm -lpthread
-#gcc -o plasma.bin source/plasma.c source/dos.c -lSDL2 -lGLEW -lGL -lm -lpthread
-#gcc -o raycast.bin source/raycast.c source/dos.c -lSDL2 -lGLEW -lGL -lm -lpthread
-#gcc -o rotozoom.bin source/rotozoom.c source/dos.c -lSDL2 -lGLEW -lGL -lm -lpthread
-#gcc -o stranded.bin source/stranded.c source/dos.c -lSDL2 -lGLEW -lGL -lm -lpthread
-#gcc -o tracker.bin source/tracker.c source/dos.c -lSDL2 -lGLEW -lGL -lm -lpthread
-#gcc -o tunnel.bin source/tunnel.c source/dos.c -lSDL2 -lGLEW -lGL -lm -lpthread
-#gcc -o voxel.bin source/voxel.c source/dos.c -lSDL2 -lGLEW -lGL -lm -lpthread
+gcc -o julia.bin source/julia.c source/dos.c -lSDL2 -lGLEW -lGL -lm -lpthread
+gcc -o mandelbrot.bin source/mandelbrot.c source/dos.c -lSDL2 -lGLEW -lGL -lm -lpthread
+gcc -o plasma.bin source/plasma.c source/dos.c -lSDL2 -lGLEW -lGL -lm -lpthread
+gcc -o raycast.bin source/raycast.c source/dos.c -lSDL2 -lGLEW -lGL -lm -lpthread
+gcc -o rotozoom.bin source/rotozoom.c source/dos.c -lSDL2 -lGLEW -lGL -lm -lpthread
+gcc -o stranded.bin source/stranded.c source/dos.c -lSDL2 -lGLEW -lGL -lm -lpthread
+gcc -o tracker.bin source/tracker.c source/dos.c -lSDL2 -lGLEW -lGL -lm -lpthread
+gcc -o tunnel.bin source/tunnel.c source/dos.c -lSDL2 -lGLEW -lGL -lm -lpthread
+gcc -o voxel.bin source/voxel.c source/dos.c -lSDL2 -lGLEW -lGL -lm -lpthread
