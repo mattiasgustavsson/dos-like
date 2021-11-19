a429f05c5176c5a33176d8307280c0eced675f87
Bernhard Schelling <14200249+schellingb@users.noreply.github.com>
2021-11-19 23:54:47 +0900

    Change dos.c to compile as c11 Fixes typedef-redefinition warnings in opblib.h

--- wasm/build_all.bat
+++ wasm/build_all.bat
@@ -1,4 +1,4 @@
-node wajicup.js ../source/dos.c dos.o
+node wajicup.js -cc -std=c11 ../source/dos.c dos.o
 node wajicup.js -stacksize 524288 -ld dos.o ../source/burn.c burn.js
 node wajicup.js -stacksize 524288 -ld dos.o -args -embed edit.c ../source/edit.c ../source/edit.c edit.js
 node wajicup.js -stacksize 524288 -ld dos.o ../source/julia.c julia.js
@@ -6,7 +6,7 @@ node wajicup.js -stacksize 524288 -ld dos.o ../source/mandelbrot.c mandelbrot.js
 node wajicup.js -stacksize 524288 -ld dos.o ../source/plasma.c plasma.js
 node wajicup.js -stacksize 524288 -ld dos.o -embed files/raycast/ ../files/raycast/ ../source/raycast.c raycast.js
 node wajicup.js -stacksize 524288 -ld dos.o -embed files/rotozoom.gif ../files/rotozoom.gif ../source/rotozoom.c rotozoom.js
-node wajicup.js -stacksize 524288 -ld dos.o -embed files/doom.mus ../files/doom.mus -embed files/simon.mid ../files/simon.mid -embed files/cfodder.mod ../files/cfodder.mod -embed files/soundcard.wav ../files/soundcard.wav -embed files/doom.op2 ../files/doom.op2 ../source/sound.c sound.js
+node wajicup.js -stacksize 524288 -ld dos.o -embed files/doom.mus ../files/doom.mus -embed files/simon.mid ../files/simon.mid -embed files/cfodder.mod ../files/cfodder.mod -embed files/soundcard.wav ../files/soundcard.wav -embed files/doom.op2 ../files/doom.op2 -embed files/doom.opb ../files/doom.opb ../source/sound.c sound.js
 node wajicup.js -stacksize 524288 -ld dos.o -embed files/stranded/ ../files/stranded/ ../source/stranded.c stranded.js
 node wajicup.js -stacksize 524288 -ld dos.o ../source/tracker.c tracker.js
 node wajicup.js -stacksize 524288 -ld dos.o -embed files/tunnel.gif ../files/tunnel.gif ../source/tunnel.c tunnel.js
